5d0a611107b8e3ba85a4549664384c2b2a35be22
Dimitri Kouvdis <dimitri@metaplatforms.com>
2021-08-18 23:26:51 +0300

    init commit

---
+++ go.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+const go = () => {
+  const secrets = process.env.SECRETS_CONTEXT;
+
+  if (!secrets) {
+    console.log("Missing SECRETS_CONTEXT");
+  }
+
+  const logger = fs.createWriteStream(".env", {
+    flags: "a",
+  });
+
+  for (const [key, value] of Object.entries(secrets)) {
+    logger.write(`${key}=${value}\n`);
+  }
+};
